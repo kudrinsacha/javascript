559853abd1b056461f8796a10134e6160b456542
Alexandr Kudrin <8096304@gmail.com>
2024-01-29 21:19:12 +0300

    added resize listener

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
-if (window.innerWidth < 768) {
-    const swiper = new Swiper('.swiper', {
-        spaceBetween: 16,
-        centeredSlides: true,
-        slidesOffsetBefore: 16,
-        width: 240,
-        height: 72,
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-        },
-    });
+let swiper;
+
+function slider() {
+    if (window.innerWidth < 768) {
+        const buttonShow = document.querySelector('.swiper-show')
+        const slides = document.querySelectorAll('.swiper-slide:nth-child(n + 7)');
+        const slidesHidden = document.querySelectorAll('.swiper-slide:nth-child(n + 9)');
+        swiper = new Swiper('.swiper', {
+            spaceBetween: 16,
+            slidesOffsetBefore: 16,
+            width: 240,
+            height: 72,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true
+            },
+        });
+        buttonShow.classList.add('hidden')
+        slides.forEach((slide) => {
+            slide.classList.remove('hidden');
+        });
+        slidesHidden.forEach((slide) => {
+            slide.classList.add('hidden');
+        });
+    }
+
+    if (window.innerWidth > 767) {
+        const slides = document.querySelectorAll('.swiper-slide:nth-child(n + 7)');
+        const buttonShow = document.querySelector('.swiper-show')
+        slides.forEach((slide) => {
+            slide.classList.add('hidden');
+        });
+        buttonShow.classList.remove('hidden')
+        if (document.querySelector(".swiper").classList.contains("swiper-initialized")) {
+            swiper.destroy();
+        }
+    }
+
+    if (window.innerWidth > 1119) {
+        const slides = document.querySelectorAll('.swiper-slide:nth-child(-n + 8)');
+        slides.forEach((slide) => {
+            slide.classList.remove('hidden');
+        });
+    }
+
+    if (window.innerWidth > 1359) {
+        const slides = document.querySelectorAll('.swiper-slide')
+        const buttonShow = document.querySelector('.swiper-show')
+        slides.forEach((slide) => {
+            slide.classList.remove('hidden')
+        })
+        buttonShow.classList.add('hidden')
+    }
 }
 
-if (window.innerWidth >= 768) {
-    const slides = document.querySelectorAll('.swiper-slide:nth-child(n + 7)');
-    const buttonShow = document.querySelector('.swiper-show')
-    slides.forEach((slide) => {
-        slide.classList.add('hidden');
-    });
-    buttonShow.classList.remove('hidden')
-}
-
-if (window.innerWidth >= 1120) {
-    const slides = document.querySelectorAll('.swiper-slide:nth-child(-n + 8)');
-    slides.forEach((slide) => {
-        slide.classList.remove('hidden');
-    });
-}
-
-if (window.innerWidth >= 1360) {
-    const slides = document.querySelectorAll('.swiper-slide')
-    const buttonShow = document.querySelector('.swiper-show')
-    slides.forEach((slide) => {
-        slide.classList.remove('hidden')
-    })
-    buttonShow.classList.add('hidden')
-}
 
 const buttonShow = document.querySelector('.swiper-show');
 
@@ -49,12 +66,12 @@ buttonShow.onclick = function () {
 
         buttonShow.classList.remove('active');
 
-        if (window.innerWidth >= 768 && window.innerWidth < 1120) {
+        if (window.innerWidth > 767 && window.innerWidth < 1120) {
             const hiddenSlides = document.querySelectorAll('.swiper-slide:nth-child(n+7)')
             hiddenSlides.forEach((slide) => {
                 slide.classList.add('hidden');
             });
-        } else if (window.innerWidth >= 1120) {
+        } else if (window.innerWidth > 1119) {
             const hiddenSlides = document.querySelectorAll('.swiper-slide:nth-child(n+9)')
             hiddenSlides.forEach((slide) => {
                 slide.classList.add('hidden');
@@ -78,3 +95,6 @@ buttonShow.onclick = function () {
 
     }
 };
+
+slider();
+window.addEventListener('resize', slider);
